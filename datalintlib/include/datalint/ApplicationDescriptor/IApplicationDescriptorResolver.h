a7deb8414980b68dd1925d1ce0f0a9d1d901ebf7
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-01-02 13:28:10 -0500

    Fix documentation typo

--- datalintlib/include/datalint/ApplicationDescriptor/IApplicationDescriptorResolver.h
+++ datalintlib/include/datalint/ApplicationDescriptor/IApplicationDescriptorResolver.h
@@ -17,7 +17,7 @@ class IApplicationDescriptorResolver {
 
   /// @brief Resolve the application descriptor (name and version) from the given raw data.
   /// @param rawData The raw data from which to resolve the application descriptor.
-  /// @return The resolved ApplicationDescriptor.
+  /// @return The result of the resolution, including any errors encountered.
   virtual ResolveResult Resolve(const datalint::RawData& rawData) = 0;
 };
 }  // namespace datalint
