c6cf906e0c135d173959c164f5d18d817457d3fe
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-01-02 02:29:08 -0500

    Update main executable

--- tests/src/CsvFileParserTests.cpp
+++ tests/src/CsvFileParserTests.cpp
@@ -12,28 +12,33 @@ TEST(CsvFileParserTest, CanInitializeWithFields) {
   const std::string tempCsvFile = "temp_test.csv";
   {
     std::ofstream outFile(tempCsvFile);
-    outFile << "key1,value1\n";
-    outFile << "key2,value2\n";
-    outFile << "key1,value3\n";
+    outFile << "key1,value1,Simple two-column row\n";
+    outFile << "key2 , value2 , Leading and trailing spaces (unquoted) \n";
+    outFile << "\"k,ey3\",\"val,ue3\",\"Embedded commas inside quoted fields\"\n";
+    outFile << "\"key\"\"4\",\"value\"\"4\",\"Embedded double quotes (escaped by doubling)\"\n";
   }
   datalint::input::CsvFileParser parser;
   const datalint::RawData rawData = parser.Parse(tempCsvFile);
   const auto& fields = rawData.Fields();
-  ASSERT_EQ(fields.size(), 3);
+  ASSERT_EQ(fields.size(), 4);
   // Order of occurrence and key to value relationship should both be preserved
   EXPECT_EQ(fields[0].Key, "key1");
-  EXPECT_EQ(fields[0].Value, "value1");
+  EXPECT_EQ(fields[0].Value, "value1,Simple two-column row");
   EXPECT_EQ(fields[0].Location.Filename, tempCsvFile);
   EXPECT_EQ(fields[0].Location.Line, 1);
   EXPECT_EQ(fields[1].Key, "key2");
-  EXPECT_EQ(fields[1].Value, "value2");
+  EXPECT_EQ(fields[1].Value, "value2,Leading and trailing spaces (unquoted)");
   EXPECT_EQ(fields[1].Location.Filename, tempCsvFile);
   EXPECT_EQ(fields[1].Location.Line, 2);
-  EXPECT_EQ(fields[2].Key, "key1");
-  EXPECT_EQ(fields[2].Value, "value3");
+  EXPECT_EQ(fields[2].Key, "\"k,ey3\"");
+  EXPECT_EQ(fields[2].Value, "\"val,ue3\",\"Embedded commas inside quoted fields\"");
   EXPECT_EQ(fields[2].Location.Filename, tempCsvFile);
   EXPECT_EQ(fields[2].Location.Line, 3);
+  EXPECT_EQ(fields[3].Key, "\"key\"4\"");
+  EXPECT_EQ(fields[3].Value, "\"value\"4\",\"Embedded double quotes (escaped by doubling)\"");
+  EXPECT_EQ(fields[3].Location.Filename, tempCsvFile);
+  EXPECT_EQ(fields[3].Location.Line, 4);
 
   int result = std::remove(tempCsvFile.c_str());
-  ASSERT_EQ(result, 0);  // optional check that deletion succeeded
+  ASSERT_EQ(result, 0);  // optional check that deletion of temporary file succeeded
 }
