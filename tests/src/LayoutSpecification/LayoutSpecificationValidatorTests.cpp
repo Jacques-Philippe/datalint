35e70d10f349b02d9095781ca65d9ed9937c2acf
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-01-11 06:17:41 -0500

    Define tests for validating occurrence of fields

--- tests/src/LayoutSpecification/LayoutSpecificationValidatorTests.cpp
+++ tests/src/LayoutSpecification/LayoutSpecificationValidatorTests.cpp
@@ -45,3 +45,121 @@ TEST(LayoutSpecificationValidatorTest, CanInitializeWithValidFields) {
   ASSERT_TRUE(isValid);
   ASSERT_TRUE(errorCollector.GetErrorLogs().empty());
 }
+
+/// @brief Tests that the layout specification validator detects missing required fields
+TEST(LayoutSpecificationValidatorTest, DetectsMissingRequiredFields) {
+  datalint::error::ErrorCollector errorCollector;
+  datalint::layout::LayoutSpecificationValidator validator{
+      datalint::layout::UnexpectedFieldStrictness::Strict};
+
+  // Create some raw data with fields
+  const datalint::RawData rawData({
+      datalint::RawField{"Field1", "Value1, Value2, Value3"},
+  });
+
+  // Create some expected fields
+  const std::map<std::string, datalint::layout::ExpectedField> expectedFields = {
+      {"Field1", datalint::layout::ExpectedField{1, std::nullopt}},
+      {"Field2", datalint::layout::ExpectedField{1, std::nullopt}}};
+
+  // Initialize the layout specification
+  datalint::layout::LayoutSpecification layoutSpecification(expectedFields);
+
+  // Validate the layout specification against the raw data
+  const bool isValid = validator.Validate(layoutSpecification, rawData, errorCollector);
+  ASSERT_FALSE(isValid);
+  const auto errorLogs = errorCollector.GetErrorLogs();
+  ASSERT_EQ(errorLogs.size(), 1);
+  ASSERT_EQ(errorLogs[0].Subject(), "Missing Required Field");
+  ASSERT_NE(errorLogs[0].Body().find("Field2"), std::string::npos);
+}
+
+/// @brief Tests that the layout specification validator detects extra (too many) occurrences of
+/// fields
+TEST(LayoutSpecificationValidatorTest, DetectsTooManyFieldOccurrences) {
+  datalint::error::ErrorCollector errorCollector;
+  datalint::layout::LayoutSpecificationValidator validator{
+      datalint::layout::UnexpectedFieldStrictness::Strict};
+
+  // Create some raw data with fields
+  const datalint::RawData rawData({
+      datalint::RawField{"Field1", "Value1"},
+      datalint::RawField{"Field1", "Value2"},
+      datalint::RawField{"Field2", "ValueA"},
+  });
+
+  // Create some expected fields
+  const std::map<std::string, datalint::layout::ExpectedField> expectedFields = {
+      {"Field1", datalint::layout::ExpectedField{1, 1}},
+      {"Field2", datalint::layout::ExpectedField{1, std::nullopt}}};
+
+  // Initialize the layout specification
+  datalint::layout::LayoutSpecification layoutSpecification(expectedFields);
+
+  // Validate the layout specification against the raw data
+  const bool isValid = validator.Validate(layoutSpecification, rawData, errorCollector);
+  ASSERT_FALSE(isValid);
+  const auto errorLogs = errorCollector.GetErrorLogs();
+  ASSERT_EQ(errorLogs.size(), 1);
+  ASSERT_EQ(errorLogs[0].Subject(), "Duplicate Field");
+  ASSERT_NE(errorLogs[0].Body().find("Field1"), std::string::npos);
+}
+
+/// @brief Tests that the layout specification validator detects unexpected fields when in strict
+/// mode
+TEST(LayoutSpecificationValidatorTest, DetectsUnexpectedFieldsInStrictMode) {
+  datalint::error::ErrorCollector errorCollector;
+  datalint::layout::LayoutSpecificationValidator validator{
+      datalint::layout::UnexpectedFieldStrictness::Strict};
+
+  // Create some raw data with fields
+  const datalint::RawData rawData({
+      datalint::RawField{"Field1", "Value1"},
+      datalint::RawField{"Field2", "ValueA"},
+      datalint::RawField{"UnexpectedField", "Surprise!"},
+  });
+
+  // Create some expected fields
+  const std::map<std::string, datalint::layout::ExpectedField> expectedFields = {
+      {"Field1", datalint::layout::ExpectedField{1, std::nullopt}},
+      {"Field2", datalint::layout::ExpectedField{1, std::nullopt}}};
+
+  // Initialize the layout specification
+  datalint::layout::LayoutSpecification layoutSpecification(expectedFields);
+
+  // Validate the layout specification against the raw data
+  const bool isValid = validator.Validate(layoutSpecification, rawData, errorCollector);
+  ASSERT_FALSE(isValid);
+  const auto errorLogs = errorCollector.GetErrorLogs();
+  ASSERT_EQ(errorLogs.size(), 1);
+  ASSERT_EQ(errorLogs[0].Subject(), "Unexpected Field");
+  ASSERT_NE(errorLogs[0].Body().find("UnexpectedField"), std::string::npos);
+}
+
+/// @brief Tests that the layout specification validator will not detect unexpected fields when in
+/// permissive mode
+TEST(LayoutSpecificationValidatorTest, IgnoresUnexpectedFieldsInPermissiveMode) {
+  datalint::error::ErrorCollector errorCollector;
+  datalint::layout::LayoutSpecificationValidator validator{
+      datalint::layout::UnexpectedFieldStrictness::Permissive};
+
+  // Create some raw data with fields
+  const datalint::RawData rawData({
+      datalint::RawField{"Field1", "Value1"},
+      datalint::RawField{"Field2", "ValueA"},
+      datalint::RawField{"UnexpectedField", "Surprise!"},
+  });
+
+  // Create some expected fields
+  const std::map<std::string, datalint::layout::ExpectedField> expectedFields = {
+      {"Field1", datalint::layout::ExpectedField{1, std::nullopt}},
+      {"Field2", datalint::layout::ExpectedField{1, std::nullopt}}};
+
+  // Initialize the layout specification
+  datalint::layout::LayoutSpecification layoutSpecification(expectedFields);
+
+  // Validate the layout specification against the raw data
+  const bool isValid = validator.Validate(layoutSpecification, rawData, errorCollector);
+  ASSERT_TRUE(isValid);
+  ASSERT_TRUE(errorCollector.GetErrorLogs().empty());
+}
