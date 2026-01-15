0be250b7c479dd2d7fd963b2db3f9ba65d9a2849
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-01-15 05:51:09 -0500

    Define StringUtils with tests

---
+++ datalintlib/include/datalint/RuleSpecification/IValueRule.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <datalint/Error/ErrorCollector.h>
+#include <datalint/RuleSpecification/RuleContext.h>
+
+namespace datalint::rules {
+/// @brief Class responsible for specifying a rule to apply to a value
+class IValueRule {
+ public:
+  /// @brief Virtual destructor
+  virtual ~IValueRule() = default;
+
+  /// @brief Virtual function to define how the rule should evaluate
+  /// @param ctx the context of the rule
+  /// @param errorCollector the error collector
+  virtual void Evaluate(const RuleContext& ctx,
+                        datalint::error::ErrorCollector& errorCollector) const = 0;
+};
+}  // namespace datalint::rules
---
+++ datalintlib/include/datalint/RuleSpecification/IntegerInRangeRule.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <datalint/Error/ErrorLog.h>
+#include <datalint/RuleSpecification/IValueRule.h>
+#include <datalint/StringUtils.h>
+
+namespace datalint::rules {
+class IntegerInRangeRule final : public IValueRule {
+ public:
+  IntegerInRangeRule(int min, int max) : Min_(min), Max_(max) {}
+
+  void Evaluate(const RuleContext& ctx,
+                datalint::error::ErrorCollector& errorCollector) const override {
+    int value;
+    if (!datalint::utils::TryParseInt(ctx.Value.Value, value)) {
+      errorCollector.AddErrorLog(
+          datalint::error::ErrorLog{"Incorrect value type", "Value must be an integer"});
+      return;
+    }
+
+    if (value < Min_ || value > Max_) {
+      errorCollector.AddErrorLog(datalint::error::ErrorLog{
+          "Incorrect value",
+          "Value must be between " + std::to_string(Min_) + " and " + std::to_string(Max_)});
+    }
+  }
+
+ private:
+  int Min_;
+  int Max_;
+};
+}  // namespace datalint::rules
