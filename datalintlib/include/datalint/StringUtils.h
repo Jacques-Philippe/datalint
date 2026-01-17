c901ca7d16f6e1c7020f9049f4d4252fe8f4bbb0
Jacques-Philippe Amiot <jacques.p.amiot@gmail.com>
2026-01-17 04:10:29 -0500

    Add nodiscard to StringUtils

--- datalintlib/include/datalint/StringUtils.h
+++ datalintlib/include/datalint/StringUtils.h
@@ -6,5 +6,6 @@ namespace datalint::utils {
 /// @param text the text to parse
 /// @param out the outgoing int value
 /// @return true for the parse operation was successful
+[[nodiscard]]
 bool TryParseInt(const std::string_view text, int& out);
 }  // namespace datalint::utils
